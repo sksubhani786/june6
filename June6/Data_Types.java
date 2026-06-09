b17172785a6a874a1a6722a681819216891906fc
subhani <sksubhani550@gmail.com>
2026-06-09 09:33:16 +0530

    first commit

---
+++ June6/Data_Types.java
@@ -0,0 +1,29 @@
+package June6;
+
+public class Data_Types {
+
+	public static void main(String[] args) 
+	{
+		byte by = 100;   //-128 to 127
+		short sh =500;  //-32768 to 32767
+		int i= 3200;    //-214483648 to 214483648
+		long l= 97845126l;
+		float fl = 23.05f;
+		double db = 1542.78459;
+		char ch = 'A';
+		boolean bl=false;
+		
+		System.out.println("Byte value is:"+by);
+		System.out.println("short value is:"+sh);
+		System.out.println("int value is:"+i);
+		System.out.println("long value is:"+l);
+		System.out.println("float value is:"+fl);
+		System.out.println("double value is:"+db);
+		System.out.println("char value is:"+ch);
+		System.out.println("boolean value is:"+bl);
+		
+		
+		
+	}
+
+}
